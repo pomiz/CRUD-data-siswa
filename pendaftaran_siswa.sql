7faf9b5211b184a6e42a572255abd21572781687
pomiz <azdkaprattama22@gmail.com>
2023-07-06 09:19:03 +0700

    data siswa

---
+++ pendaftaran_siswa.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Jul 2023 pada 04.17
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `calon_siswa`
+--
+
+CREATE TABLE `calon_siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(60) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(60) NOT NULL,
+  `agama` varchar(60) NOT NULL,
+  `sekolah_asal` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `calon_siswa`
+--
+
+INSERT INTO `calon_siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`) VALUES
+(1, 'azdka', 'kalimalang', 'Pria', 'Islam', 'SMPN 117'),
+(2, 'fira', 'slipi', 'perempuan', 'Islam', 'makassar'),
+(6, 'oji', 'pasar minggu', 'laki-laki', 'Islam', 'SMPN 51');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'admin', 'admin@gmail.com', '12345'),
+(3, 'azdka', 'azdkaprt@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(4, 'ka', 'ka@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
